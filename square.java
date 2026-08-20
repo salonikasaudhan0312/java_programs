67a7a877664f2531d77d0c2736eed38b6fe8c3ec
Saloni <saloni.kasaudhan0312@gmail.com>
2026-08-20 15:40:08 +0530

    square commit

---
+++ square.java
@@ -0,0 +1,14 @@
+public class square {
+  
+	public static void main(String[]args)
+	{
+		int s=square(3,4);
+		System.out.print("addition="+s);
+	}
+	static int square(int a,int b  )
+	{
+		return a*a+b*b;
+	}
+}
+  
+
