f296fead157a4962548a250327c2085729ff147c
Saloni <saloni.kasaudhan0312@gmail.com>
2026-08-31 21:11:56 +0530

    starsquare

--- NumberSquareP.java
+++ NumberSquareP.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
 
 public class NumberSquareP {
-  public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter a number");
-        int n = sc.nextInt();
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                System.out.print(j+" ");
-            }
-            System.out.println();
-        }
+  // public static void main(String[] args) {
+  //       Scanner sc = new Scanner(System.in);
+  //       System.out.println("enter a number");
+  //       int n = sc.nextInt();
+  //       for(int i=1; i<=n; i++){
+  //           for(int j=1; j<=n; j++){
+  //               System.out.print(j+" ");
+  //           }
+  //           System.out.println();
+  //       }
+  //   }
+
+
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter a number");
+    int n = sc.nextInt();
+    for(int i = 1; i<=n; i++){
+      for(int j=1; j<=n; j++){
+        System.out.print((char)(j+64)+" ");
+      }
+      System.out.println();
     }
+  }
   
 }
--- StarSquare.java
+++ StarSquare.java
@@ -1,14 +1,39 @@
 import java.util.Scanner;
 public class StarSquare {
-  public static void main(String[] args){
+  // public static void main(String[] args){
+  //   Scanner sc = new Scanner(System.in);
+  //   System.out.println("enter a number");
+  //   int n = sc.nextInt();
+  //   for(int i=1; i<=n; i++){
+  //     for(int j=1; j<=n; j++){
+  //       System.out.print("*");
+  //     }
+  //     System.out.println();
+  //   }
+  // }
+
+  // public static void main(String[] args){
+  //   Scanner sc = new Scanner(System.in);
+  //   System.out.println("enter a number");
+  //   int n = sc.nextInt();
+  //   for(int i=1; i<=n; i++){
+  //     for(int j=1; j<=n; j++){
+  //       System.out.print((char)(j+64)+" ");
+  //     }
+  //     System.out.println();
+  //   }
+  // }
+  
+
+   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
+    System.out.println("enter a number");
     int n = sc.nextInt();
     for(int i=1; i<=n; i++){
       for(int j=1; j<=n; j++){
-        System.out.print("*");
+        System.out.print((char)(i+64)+" ");
       }
       System.out.println();
     }
   }
-  
 }
