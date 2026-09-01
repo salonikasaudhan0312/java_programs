e4b83b63e9bdb209e62e0653fcd5af027f0f9939
Saloni <saloni.kasaudhan0312@gmail.com>
2026-09-01 22:47:14 +0530

    syntax

---
+++ Syntax.java
@@ -0,0 +1,20 @@
+public class Syntax {
+  public static void harsh(){
+    System.out.println("ashirvad");
+    for(int i=1; i<=5; i++){
+      System.out.println(i+ "");
+    }
+    
+  }
+  public static void main(String[] args){
+    System.out.println("saloni");
+    harsh();
+    trishul();
+
+  }
+  public static void trishul(){
+    System.out.println("Arpit");
+    
+  }
+  
+}
