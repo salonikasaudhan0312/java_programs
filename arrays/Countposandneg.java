207e4acf90b1520d354bc18694cb9555184c2be2
Saloni <saloni.kasaudhan0312@gmail.com>
2026-09-18 21:57:28 +0530

    Countposandneg

--- arrays/Countposandneg.java
+++ arrays/Countposandneg.java
@@ -1,4 +1,21 @@
 package arrays;
 
 public class Countposandneg {
+    public static void main(String[] args) {
+        int[] arr = {10, -5, 20, -8, 15, -2};
+
+        int positive = 0;
+        int negative = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 0) {
+                positive++;
+            } else {
+                negative++;
+            }
+        }
+
+        System.out.println("Positive = " + positive);
+        System.out.println("Negative = " + negative);
+    }
 }
