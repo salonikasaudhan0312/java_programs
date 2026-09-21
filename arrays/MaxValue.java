a310b8c1367e23763bb66addbf747b55c3957c75
Saloni <saloni.kasaudhan0312@gmail.com>
2026-09-21 21:00:37 +0530

    maxvalue

--- arrays/MaxValue.java
+++ arrays/MaxValue.java
@@ -1,4 +1,16 @@
 package arrays;
 
 public class MaxValue {
+    static void main() {
+        int arr[] = {3,5,-2,8,9};
+        int n = arr.length;
+        int maxValue = arr[0];
+        for(int i = 0; i<=n-1; i++){
+            if(arr[i] > maxValue){
+                maxValue = arr[i];
+
+            }
+        }
+        System.out.println(maxValue);
+    }
 }
--- arrays/multiply.java
+++ arrays/multiply.java
@@ -1,4 +1,15 @@
 package arrays;
 
 public class multiply {
+    public static void main(String[] args) {
+        int arr[] = {2, 3, 4, 67};
+        int ans = 1;
+        int n = arr.length;
+        for (int i = 0; i <= n - 1; i++) {
+            int value = arr[i];
+            ans = ans * value;
+
+        }
+        System.out.println(ans);
+    }
 }
