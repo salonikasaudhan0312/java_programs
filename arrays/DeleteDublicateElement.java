85235a7f8ce8db9381d02f93cab8d62773187680
Saloni <saloni.kasaudhan0312@gmail.com>
2026-09-21 21:29:19 +0530

    deletedublicateelement

--- arrays/DeleteDublicateElement.java
+++ arrays/DeleteDublicateElement.java
@@ -1,4 +1,20 @@
 package arrays;
 
 public class DeleteDublicateElement {
+    static void main() {
+        int[] arr = {1, 1, 2, 2, 3, 4, 4};
+
+        int j = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+
+        for (int i = 0; i <= j; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
--- arrays/SecondLargestElement.java
+++ arrays/SecondLargestElement.java
@@ -1,4 +1,21 @@
 package arrays;
 
 public class SecondLargestElement {
+    static void main() {
+        int[] arr = {10, 5, 20, 8, 15};
+
+        int max = arr[0];
+        int second = Integer.MIN_VALUE;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                second = max;
+                max = arr[i];
+            } else if (arr[i] > second && arr[i] != max) {
+                second = arr[i];
+            }
+        }
+
+        System.out.println(second);
+    }
 }
