7e4453470bf6c02374ada5424941242c1b56ae42
Saloni <saloni.kasaudhan0312@gmail.com>
2026-09-21 21:20:58 +0530

    arrayreverse

--- arrays/ArrayReverse.java
+++ arrays/ArrayReverse.java
@@ -1,4 +1,24 @@
 package arrays;
 
 public class ArrayReverse {
+    static void main() {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
 }
