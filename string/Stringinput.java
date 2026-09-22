a6c9fbd101978d55522aa97607c1e22caf6e7fc1
Saloni <saloni.kasaudhan0312@gmail.com>
2026-09-22 22:01:10 +0530

    compare

--- string/Stringinput.java
+++ string/Stringinput.java
@@ -1,4 +1,18 @@
-package string;
+import java.util.Scanner;
 
-public class stringinput {
+class Stringinput {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("provide the string content:");
+        String str1 = sc.nextLine();
+
+        System.out.println("value is nextline: " + str1);
+
+        System.out.println("provide the string content:");
+        String str2 = sc.next();
+
+        System.out.println("value is next: " + str2);
+    }
 }
--- string/compare.java
+++ string/compare.java
@@ -1,4 +1,20 @@
 package string;
 
 public class compare {
+    static void main() {
+
+
+        String name1 = "SALONI";
+        String name2 = "SALONI";
+        if (name1.equals(name2)) {
+            System.out.println("both strings are equals");
+
+
+        } else {
+            System.out.println("both strings are equals");
+
+
+        }
+    }
+
 }
