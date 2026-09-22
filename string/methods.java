e2d55466d8014511140f833c7bbc47ddfd5ec486
Saloni <saloni.kasaudhan0312@gmail.com>
2026-09-22 22:59:09 +0530

    method

--- string/methods.java
+++ string/methods.java
@@ -1,4 +1,19 @@
 package string;
 
 public class methods {
+    static void main() {
+        String str = "my name is saloni";
+        System.out.println(str.length());
+        System.out.println(str.charAt(0));
+        System.out.println(str.toUpperCase());
+        System.out.println(str.substring(1, 4));
+        System.out.println(str.contains("is"));
+        System.out.println(str.startsWith("my"));
+        System.out.println(str.endsWith("saloni"));
+//        String str = "  ";
+//        System.out.println(str.isEmpty());
+//        System.out.println(str.isBlank());
+
+
+    }
 }
