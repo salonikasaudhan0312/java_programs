3ff4218381eb52543ea5bdfd66f2fda297cdc853
Saloni <saloni.kasaudhan0312@gmail.com>
2026-09-26 18:22:59 +0530

    CountSpace

--- string/CountSpace.java
+++ string/CountSpace.java
@@ -1,4 +1,20 @@
 package string;
 
 public class CountSpace {
-}
+        public static void main(String[] args) {
+
+            String str = "Hello World Java";
+
+            int count = 0;
+
+            for (int i = 0; i < str.length(); i++) {
+
+                if (str.charAt(i) == ' ') {
+                    count++;
+                }
+            }
+
+            System.out.println("Spaces = " + count);
+        }
+    }
+
